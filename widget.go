fda69da602c35b561f696206513335ef4763c09c
Christian Muehlhaeuser <muesli@gmail.com>
2019-07-04 11:13:11 +0200

    Added drawString helper method

--- widget.go
+++ widget.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"image"
+	"image/color"
 	"io/ioutil"
 	"log"
 	"strconv"
@@ -8,10 +10,14 @@ import (
 	"github.com/golang/freetype"
 	"github.com/golang/freetype/truetype"
 	"github.com/muesli/streamdeck"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
 )
 
 var (
-	ttfFont *truetype.Font
+	ttfFont     *truetype.Font
+	ttfThinFont *truetype.Font
+	ttfBoldFont *truetype.Font
 )
 
 type Widget interface {
@@ -53,13 +59,69 @@ func NewWidget(index uint8, id string, action *ActionConfig, config map[string]s
 	return nil
 }
 
+func drawString(img *image.RGBA, ttf *truetype.Font, text string, fontsize float64, pt fixed.Point26_6) {
+	c := freetype.NewContext()
+	c.SetDPI(72)
+	c.SetFont(ttf)
+	c.SetSrc(image.NewUniform(color.RGBA{0, 0, 0, 0}))
+	c.SetDst(img)
+	c.SetClip(img.Bounds())
+	c.SetHinting(font.HintingNone)
+	c.SetFontSize(fontsize)
+
+	// find text entent
+	c.SetSrc(image.NewUniform(color.RGBA{0, 0, 0, 0}))
+	extent, _ := c.DrawString(text, freetype.Pt(0, 0))
+	actwidth := int(float64(extent.X) / 64)
+	actheight := c.PointToFixed(fontsize/2.0) / 64
+	xcenter := (float64(img.Bounds().Dx()) / 2.0) - (float64(actwidth) / 2.0)
+	ycenter := (float64(58) / 2.0) + (float64(actheight) / 2.0)
+
+	if pt.X < 0 {
+		oldy := pt.Y
+		pt = freetype.Pt(int(xcenter), 0)
+		pt.Y = oldy
+	}
+	if pt.Y < 0 {
+		oldx := pt.X
+		pt = freetype.Pt(0, int(ycenter))
+		pt.X = oldx
+	}
+
+	c.SetSrc(image.NewUniform(color.RGBA{255, 255, 255, 255}))
+	_, err := c.DrawString(text, pt)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func init() {
-	ttf, err := ioutil.ReadFile("/usr/share/fonts/TTF/Roboto-Medium.ttf")
+	ttf, err := ioutil.ReadFile("/usr/share/fonts/TTF/Roboto-Regular.ttf")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	ttfFont, err = freetype.ParseFont(ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ttf, err = ioutil.ReadFile("/usr/share/fonts/TTF/Roboto-Thin.ttf")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ttfThinFont, err = freetype.ParseFont(ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ttf, err = ioutil.ReadFile("/usr/share/fonts/TTF/Roboto-Bold.ttf")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ttfBoldFont, err = freetype.ParseFont(ttf)
 	if err != nil {
 		log.Fatal(err)
 	}
