43afa219ba342b4a3739735ae48b80082f65459f
Christian Muehlhaeuser <muesli@gmail.com>
2021-05-29 23:10:28 +0200

    Improve time widget rendering
    - Support multiple placeholders when formatting time string
    - Automatically pick matching font size

--- widget_time.go
+++ widget_time.go
@@ -3,75 +3,49 @@ package main
 import (
 	"image"
 	"log"
-	"time"
 	"strings"
+	"time"
 
 	"github.com/muesli/streamdeck"
-	"github.com/golang/freetype/truetype"
 )
 
 type TimeWidget struct {
 	BaseWidget
+
 	format string
 	font   string
 }
 
-func mapToFont(font string) *truetype.Font {
-	switch font {
-	case "thin":
-		return ttfThinFont
-	case "regular":
-		return ttfFont
-	case "bold":
-		return ttfBoldFont
-	default:
-		return ttfFont
+func formatTime(t time.Time, format string) string {
+	tm := map[string]string{
+		"%Y": "2006",
+		"%y": "06",
+		"%F": "January",
+		"%M": "Jan",
+		"%m": "01",
+		"%l": "Monday",
+		"%D": "Mon",
+		"%d": "02",
+		"%h": "03",
+		"%H": "15",
+		"%i": "04",
+		"%s": "05",
+		"%a": "PM",
+		"%t": "MST",
 	}
-}
 
-func mapToTimeString(format string) string {
-	t := time.Now()
-	switch format {
-	case "yyyy":
-		return t.Format("2006")
-	case "yy":
-		return t.Format("06")
-	case "mmmm":
-		return t.Format("January")
-	case "mmm":
-		return t.Format("Jan")
-	case "mm":
-		return t.Format("01")
-	case "dddd":
-		return t.Format("Monday")
-	case "ddd":
-		return t.Format("Mon")
-	case "dd":
-		return t.Format("02")
-	case "HHT":
-		return t.Format("03")
-	case "HH", "hour":
-		return t.Format("15")
-	case "MM", "min":
-		return t.Format("04")
-	case "ss", "SS", "sec":
-		return t.Format("05")
-	case "tt":
-		return t.Format("PM")
-	case "Z", "ZZZ":
-		return t.Format("MST")
-	case "o":
-		return t.Format("Z07:00")
-	default:
-		return t.Format(format)
+	for k, v := range tm {
+		format = strings.ReplaceAll(format, k, v)
 	}
+
+	return t.Format(format)
 }
 
 func (w *TimeWidget) Update(dev *streamdeck.Device) {
-	const margin = 4
 	size := int(dev.Pixels)
-	img := image.NewRGBA(image.Rect(0, 0, size, size))
+	margin := size / 18
 	height := size - (margin * 2)
+	img := image.NewRGBA(image.Rect(0, 0, size, size))
 
 	formats := strings.Split(w.format, ";")
 	fonts := strings.Split(w.font, ";")
@@ -83,18 +57,16 @@ func (w *TimeWidget) Update(dev *streamdeck.Device) {
 		fonts = append(fonts, "regular")
 	}
 
-	pt := (float64(height) / float64(len(formats))) * 72.0 / float64(dev.DPI)
-
-	for i:=0; i<len(formats); i++ {
-		str := mapToTimeString(formats[i])
-		font := mapToFont(fonts[i])
-		lower := margin + (height / len(formats)) * i
-		upper := margin + (height / len(formats)) * (i + 1)
+	for i := 0; i < len(formats); i++ {
+		str := formatTime(time.Now(), formats[i])
+		font := fontByName(fonts[i])
+		lower := margin + (height/len(formats))*i
+		upper := margin + (height/len(formats))*(i+1)
 
 		drawString(img, image.Rect(0, lower, size, upper),
 			font,
 			str,
-			pt,
+			-1,
 			image.Pt(-1, -1))
 	}
 
