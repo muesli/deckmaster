cee39d09001334185de23b11d25c2a74cefcb210
Christian Muehlhaeuser <muesli@gmail.com>
2021-06-08 03:43:33 +0200

    Add missing window.go

---
+++ window.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+
+	"github.com/muesli/streamdeck"
+)
+
+func handleActiveWindowChanged(dev *streamdeck.Device, event ActiveWindowChangedEvent) {
+	log.Printf("Active window changed to %s (%d, %s)\n",
+		event.Window.Class, event.Window.ID, event.Window.Name)
+
+	// remove dupes
+	i := 0
+	for _, rw := range recentWindows {
+		if rw.ID == event.Window.ID {
+			continue
+		}
+
+		recentWindows[i] = rw
+		i++
+	}
+	recentWindows = recentWindows[:i]
+
+	keys := int(dev.Rows * dev.Columns)
+	recentWindows = append([]Window{event.Window}, recentWindows...)
+	if len(recentWindows) > keys {
+		recentWindows = recentWindows[0:keys]
+	}
+	deck.updateWidgets(dev)
+}
+
+func handleWindowClosed(dev *streamdeck.Device, event WindowClosedEvent) {
+	i := 0
+	for _, rw := range recentWindows {
+		if rw.ID == event.Window.ID {
+			continue
+		}
+
+		recentWindows[i] = rw
+		i++
+	}
+	recentWindows = recentWindows[:i]
+	deck.updateWidgets(dev)
+}
